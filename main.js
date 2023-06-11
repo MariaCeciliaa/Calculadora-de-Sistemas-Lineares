8079a42e4351b7dd37685f447cc37424ae114177
MariaCeciliaa <mariacecilia@edu.unifil.br>
2023-06-11 19:22:04 -0300

    att da funcao e logica do metodo de gauss seidel

--- main.js
+++ main.js
@@ -1,226 +1,286 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var botaoCalcular = document.getElementById('botaoCalcular');
-  botaoCalcular.addEventListener('click', function () {
-      calcularSistemaLinear();
-  });
+    var botaoCalcular = document.getElementById('botaoCalcular');
+    botaoCalcular.addEventListener('click', function () {
+        calcularSistemaLinear();
+    });
 
-  var numeroEquacoesInput = document.getElementById("numero-equacoes");
-  numeroEquacoesInput.addEventListener("change", function () {
-      var numeroEquacoes = parseInt(numeroEquacoesInput.value);
-      var coeficientesDiv = document.getElementById("coeficientes");
-      coeficientesDiv.innerHTML = "";
+    var numeroEquacoesInput = document.getElementById("numero-equacoes");
+    numeroEquacoesInput.addEventListener("change", function () {
+        var numeroEquacoes = parseInt(numeroEquacoesInput.value);
+        var coeficientesDiv = document.getElementById("coeficientes");
+        coeficientesDiv.innerHTML = "";
 
-      for (var i = 1; i <= numeroEquacoes; i++) {
-          var linhaDiv = document.createElement("div");
+        for (var i = 1; i <= numeroEquacoes; i++) {
+            var linhaDiv = document.createElement("div");
 
-          for (var j = 1; j <= numeroEquacoes; j++) {
-              var input = document.createElement("input");
-              input.type = "number";
-              input.id = "a-" + i + "-" + j;
-              linhaDiv.appendChild(input);
-          }
+            for (var j = 1; j <= numeroEquacoes + 1; j++) {
+                var input = document.createElement("input");
+                input.type = "number";
+                input.id = "equacao-" + i + "-coeficiente-" + j;
+                linhaDiv.appendChild(input);
+            }
 
-          var bInput = document.createElement("input");
-          bInput.type = "number";
-          bInput.id = "b-" + i;
-          linhaDiv.appendChild(bInput);
+            coeficientesDiv.appendChild(linhaDiv);
+        }
 
-          coeficientesDiv.appendChild(linhaDiv);
-      }
-  });
+        var gaussSeidelInputs = document.getElementById("gauss-seidel-inputs");
+        if (gaussSeidelInputs.style.display === "block") {
+            gerarInputsChuteInicial(numeroEquacoes);
+        }
+    });
 
-  function showHideGaussSeidelInputs() {
-      var opcao = document.getElementById("opcao").value;
-      var coeficientesDiv = document.getElementById("coeficientes");
+    var opcaoSelect = document.getElementById("opcao");
+    opcaoSelect.addEventListener("change", function () {
+        var gaussSeidelInputs = document.getElementById("gauss-seidel-inputs");
 
-      if (opcao === "2") {
-          var ordemMatriz = parseInt(document.getElementById("numero-equacoes").value);
-          var inputsHtml = "";
-
-          for (var i = 0; i < ordemMatriz; i++) {
-              inputsHtml += '<label>Chute inicial para x' + (i + 1) + ':</label>';
-              inputsHtml += '<input type="number" id="chute-inicial-' + i + '"><br><br>';
-          }
-
-          coeficientesDiv.innerHTML = inputsHtml;
-      } else {
-          coeficientesDiv.innerHTML = "";
-      }
-  }
-
-  document.getElementById("opcao").addEventListener("change", showHideGaussSeidelInputs);
-
-  function gaussSeidel(matriz, chuteInicial, precisao, iteracoesMaximas) {
-      var n = matriz.length;
-      var x = chuteInicial.slice();
-      var xAnterior = chuteInicial.slice();
-      var iteracao = 0;
-      var convergiu = false;
-
-      while (!convergiu && iteracao < iteracoesMaximas) {
-          for (var i = 0; i < n; i++) {
-              var soma = 0;
-              for (var j = 0; j < n; j++) {
-                  if (j !== i) {
-                      soma += matriz[i][j] * x[j];
-                  }
-              }
-              x[i] = (matriz[i][n] - soma) / matriz[i][i];
-          }
-
-          convergiu = true;
-          for (var k = 0; k < n; k++) {
-              if (Math.abs((x[k] - xAnterior[k]) / x[k]) > precisao) {
-                  convergiu = false;
-                  break;
-              }
-          }
-
-          xAnterior = x.slice();
-          iteracao++;
-      }
-
-      return {
-          solucao: x,
-          convergiu: convergiu,
-          iteracoes: iteracao
-      };
-  }
-
-
-  function calcularSistemaLinear() {
-      var opcao = parseInt(document.getElementById("opcao").value);
-      var numeroEquacoes = parseInt(document.getElementById("numero-equacoes").value);
-      var matriz = [];
-      for (var i = 1; i <= numeroEquacoes; i++) {
-          var linha = [];
-          for (var j = 1; j <= numeroEquacoes; j++) {
-              var input = document.getElementById("a-" + i + "-" + j);
-              linha.push(parseFloat(input.value));
-          }
-          var bInput = document.getElementById("b-" + i);
-          linha.push(parseFloat(bInput.value));
-          matriz.push(linha);
-      }
-
-      var etapasDiv = document.getElementById("etapas");
-      etapasDiv.innerHTML = "";
-
-      if (opcao === 1) {
-          var solucoesDiv = document.getElementById("solucoes");
-          solucoesDiv.innerHTML = "";
-
-          var etapas = eliminacaoGauss(matriz);
-
-          for (var i = 0; i < etapas[0].length; i++) {
-              var etapa = etapas[0][i];
-              var etapaDiv = document.createElement("div");
-              etapaDiv.classList.add("etapa");
-
-              var h2 = document.createElement("h2");
-              h2.textContent = "Etapa " + (i + 1);
-              etapaDiv.appendChild(h2);
-
-              var table = document.createElement("table");
-              table.classList.add("matrix-table");
-
-              for (var j = 0; j < etapa.length; j++) {
-                  var row = document.createElement("tr");
-                  for (var k = 0; k < etapa[j].length; k++) {
-                      var cell = document.createElement("td");
-                      cell.textContent = etapa[j][k];
-                      row.appendChild(cell);
-                  }
-                  table.appendChild(row);
-              }
-
-              etapaDiv.appendChild(table);
-              etapasDiv.appendChild(etapaDiv);
-          }
-
-          exibirSolucao(etapas[1]);
-      } else if (opcao === 2) {
-          var chuteInicial = [];
-          for (var i = 0; i < numeroEquacoes; i++) {
-              var input = document.getElementById("chute-inicial-" + i);
-              chuteInicial.push(parseFloat(input.value));
-          }
-          var precisao = parseFloat(document.getElementById("precisao").value);
-          var iteracoesMaximas = parseInt(document.getElementById("iteracoes-maximas").value);
-
-          var resultado = gaussSeidel(matriz, chuteInicial, precisao, iteracoesMaximas);
-
-          var solucoesDiv = document.getElementById("solucoes");
-          solucoesDiv.innerHTML = "";
-
-          var h2 = document.createElement("h2");
-          h2.textContent = "Solução";
-          solucoesDiv.appendChild(h2);
-
-          if (resultado.convergiu) {
-              for (var i = 0; i < resultado.solucao.length; i++) {
-                  var p = document.createElement("p");
-                  p.textContent = "x" + (i + 1) + " = " + resultado.solucao[i].toFixed(4);
-                  solucoesDiv.appendChild(p);
-              }
-
-              var convergiuP = document.createElement("p");
-              convergiuP.textContent = "O método convergiu em " + resultado.iteracoes + " iterações.";
-              solucoesDiv.appendChild(convergiuP);
-          } else {
-              var p = document.createElement("p");
-              p.textContent = "O método não convergiu com os parâmetros especificados.";
-              solucoesDiv.appendChild(p);
-          }
-      }
-  }
-
-  function eliminacaoGauss(matriz) {
-      var etapas = [];
-      etapas.push(matriz.map(function (linha) {
-          return linha.slice();
-      }));
-
-      var n = matriz.length;
-
-      for (var k = 0; k < n - 1; k++) {
-          for (var i = k + 1; i < n; i++) {
-              var multiplicador = matriz[i][k] / matriz[k][k];
-              for (var j = k; j < n + 1; j++) {
-                  matriz[i][j] -= multiplicador * matriz[k][j];
-              }
-          }
-          etapas.push(matriz.map(function (linha) {
-              return linha.slice();
-          }));
-      }
-
-      var solucao = [];
-
-      for (var i = n - 1; i >= 0; i--) {
-          var soma = 0;
-          for (var j = i + 1; j < n; j++) {
-              soma += matriz[i][j] * solucao[j];
-          }
-          solucao[i] = (matriz[i][n] - soma) / matriz[i][i];
-      }
-
-      return [etapas, solucao];
-  }
-
-
-  function exibirSolucao(solucao) {
-      var solucoesDiv = document.getElementById("solucoes");
-      solucoesDiv.innerHTML = "";
-
-      var h2 = document.createElement("h2");
-      h2.textContent = "Solução";
-      solucoesDiv.appendChild(h2);
-
-      for (var i = 0; i < solucao.length; i++) {
-          var p = document.createElement("p");
-          p.textContent = "x" + (i + 1) + " = " + solucao[i].toFixed(4);
-          solucoesDiv.appendChild(p);
-      }
-  }
+        if (opcaoSelect.value === "2") {
+            gaussSeidelInputs.style.display = "block";
+            var numeroEquacoes = parseInt(numeroEquacoesInput.value);
+            gerarInputsChuteInicial(numeroEquacoes);
+        } else {
+            gaussSeidelInputs.style.display = "none";
+        }
+    });
 });
+
+function gerarInputsChuteInicial(numeroEquacoes) {
+    var chuteInicialDiv = document.getElementById("chute-inicial");
+    chuteInicialDiv.innerHTML = "";
+
+    for (var i = 1; i <= numeroEquacoes; i++) {
+        var input = document.createElement("input");
+        input.type = "number";
+        input.id = "chute-" + i;
+        chuteInicialDiv.appendChild(input);
+    }
+}
+
+function calcularSistemaLinear() {
+    var opcao = document.getElementById("opcao").value;
+    var numeroEquacoes = parseInt(document.getElementById("numero-equacoes").value);
+    var coeficientes = [];
+
+    for (var i = 1; i <= numeroEquacoes; i++) {
+        var linha = [];
+
+        for (var j = 1; j <= numeroEquacoes + 1; j++) {
+            var input = document.getElementById("equacao-" + i + "-coeficiente-" + j);
+            linha.push(parseFloat(input.value));
+        }
+
+        coeficientes.push(linha);
+    }
+
+    if (opcao === "1") {
+        var etapasDiv = document.getElementById("etapas");
+        etapasDiv.innerHTML = "";
+
+        var resultado = eliminacaoDeGauss(coeficientes);
+
+        for (var i = 0; i < resultado.etapas.length; i++) {
+            var etapa = resultado.etapas[i];
+            var etapaDiv = document.createElement("div");
+            etapaDiv.classList.add("etapa");
+
+            var titulo = document.createElement("h2");
+            titulo.textContent = "Etapa " + (i + 1);
+            etapaDiv.appendChild(titulo);
+
+            var tabela = document.createElement("table");
+            tabela.classList.add("matrix-table");
+
+            for (var j = 0; j < etapa.length; j++) {
+                var linha = document.createElement("tr");
+
+                for (var k = 0; k < etapa[j].length; k++) {
+                    var valor = etapa[j][k];
+                    var coluna = document.createElement("td");
+                    coluna.textContent = isNaN(valor) ? '' : valor.toFixed(5);
+                    linha.appendChild(coluna);
+                }
+
+                tabela.appendChild(linha);
+            }
+
+            etapaDiv.appendChild(tabela);
+            etapasDiv.appendChild(etapaDiv);
+        }
+
+        var solucoesDiv = document.getElementById("solucoes");
+        solucoesDiv.innerHTML = "";
+
+        var solucoesTexto = document.createElement("p");
+        var solucoesArray = resultado.solucoes.map(function (solucao, index) {
+            return "x" + (index + 1) + " = " + solucao.toFixed(5);
+        });
+        solucoesTexto.textContent = "Soluções: " + solucoesArray.join(", ");
+        solucoesDiv.appendChild(solucoesTexto);
+    } else if (opcao === "2") {
+        var chuteInicial = [];
+
+        for (var i = 1; i <= numeroEquacoes; i++) {
+            var input = document.getElementById("chute-" + i);
+            chuteInicial.push(parseFloat(input.value));
+        }
+
+        var precisao = parseFloat(document.getElementById("precisao").value);
+        var iteracoesMaximas = parseInt(document.getElementById("iteracoes-maximas").value);
+
+        var etapasDiv = document.getElementById("etapas");
+        etapasDiv.innerHTML = "";
+
+        var resultado = gaussSeidel(coeficientes, chuteInicial, precisao, iteracoesMaximas);
+
+        for (var i = 0; i < resultado.etapas.length; i++) {
+            var etapa = resultado.etapas[i];
+            var etapaDiv = document.createElement("div");
+            etapaDiv.classList.add("etapa");
+
+            var titulo = document.createElement("h2");
+            titulo.textContent = "Iteração " + (i + 1);
+            etapaDiv.appendChild(titulo);
+
+            var tabela = document.createElement("table");
+            tabela.classList.add("matrix-table");
+
+            for (var j = 0; j < etapa.length; j++) {
+                var linha = document.createElement("tr");
+
+                for (var k = 0; k < etapa[j].length; k++) {
+                    var valor = etapa[j][k];
+                    var coluna = document.createElement("td");
+                    coluna.textContent = valor.toFixed(5);
+                    linha.appendChild(coluna);
+                }
+
+                tabela.appendChild(linha);
+            }
+
+            etapaDiv.appendChild(tabela);
+            etapasDiv.appendChild(etapaDiv);
+        }
+
+        var solucoesDiv = document.getElementById("solucoes");
+        solucoesDiv.innerHTML = "";
+
+        var solucoesTexto = document.createElement("p");
+        solucoesTexto.textContent = "Soluções: " + resultado.solucoes.map(function (solucao) {
+            return solucao.toFixed(5);
+        }).join(", ");
+        solucoesDiv.appendChild(solucoesTexto);
+    }
+}
+
+function eliminacaoDeGauss(coeficientes) {
+    var etapas = [];
+    var n = coeficientes.length;
+
+    etapas.push(JSON.parse(JSON.stringify(coeficientes))); 
+
+    for (var k = 0; k < n - 1; k++) {
+        for (var i = k + 1; i < n; i++) {
+            var multiplicador = coeficientes[i][k] / coeficientes[k][k];
+
+            for (var j = k; j < n + 1; j++) {
+                coeficientes[i][j] -= multiplicador * coeficientes[k][j];
+            }
+        }
+
+        var etapa = JSON.parse(JSON.stringify(coeficientes));
+        etapas.push(etapa);
+
+        if (etapa[k + 1][k] !== 0) {
+            var colunaZero = [];
+
+            for (var i = 0; i < n + 1; i++) {
+                colunaZero.push(0);
+            }
+
+            colunaZero[k] = 1;
+            etapa.splice(k + 1, 0, colunaZero);
+            etapas.push(etapa); 
+        }
+    }
+
+    var solucoes = [];
+    solucoes[n - 1] = coeficientes[n - 1][n] / coeficientes[n - 1][n - 1];
+
+    for (var i = n - 2; i >= 0; i--) {
+        var soma = 0;
+
+        for (var j = i + 1; j < n; j++) {
+            soma += coeficientes[i][j] * solucoes[j];
+        }
+
+        solucoes[i] = (coeficientes[i][n] - soma) / coeficientes[i][i];
+    }
+
+    return {
+        etapas: etapas,
+        solucoes: solucoes
+    };
+}
+
+
+
+
+function gaussSeidel(coeficientes, chuteInicial, precisao, iteracoesMaximas) {
+    var etapas = [];
+    var n = coeficientes.length;
+    var solucoes = chuteInicial.slice();
+
+    for (var iteracao = 0; iteracao < iteracoesMaximas; iteracao++) {
+        var iteracaoAnterior = solucoes.slice();
+
+        for (var i = 0; i < n; i++) {
+            var soma = 0;
+
+            for (var j = 0; j < n; j++) {
+                if (j !== i) {
+                    soma += coeficientes[i][j] * solucoes[j];
+                }
+            }
+
+            solucoes[i] = (coeficientes[i][n] - soma) / coeficientes[i][i];
+        }
+
+        etapas.push(iteracaoAnterior.map(function (valor, index) {
+            return [valor, solucoes[index]];
+        }));
+
+        var normaDiferenca = normaMaxima(subtrairVetores(solucoes, iteracaoAnterior));
+
+        if (normaDiferenca <= precisao) {
+            break;
+        }
+    }
+
+    return {
+        etapas: etapas,
+        solucoes: solucoes
+    };
+}
+
+function subtrairVetores(vetor1, vetor2) {
+    var resultado = [];
+
+    for (var i = 0; i < vetor1.length; i++) {
+        resultado.push(vetor1[i] - vetor2[i]);
+    }
+
+    return resultado;
+}
+
+function normaMaxima(vetor) {
+    var maximo = Math.abs(vetor[0]);
+
+    for (var i = 1; i < vetor.length; i++) {
+        var valorAbsoluto = Math.abs(vetor[i]);
+
+        if (valorAbsoluto > maximo) {
+            maximo = valorAbsoluto;
+        }
+    }
+
+    return maximo;
+}
