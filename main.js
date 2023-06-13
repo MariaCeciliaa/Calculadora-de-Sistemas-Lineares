9ee8ef300270f6041c50219b562f19045d554dd0
MariaCeciliaa <mariacecilia@edu.unifil.br>
2023-06-13 12:03:51 -0300

    att no layout do front do LU e permutacao

--- main.js
+++ main.js
@@ -93,47 +93,123 @@ function calcularSistemaLinear() {
     if (opcao === "1") {
         var etapasDiv = document.getElementById("etapas");
         etapasDiv.innerHTML = "";
-
+      
         var resultado = eliminacaoDeGauss(coeficientes);
-
-        for (var i = 0; i < resultado.etapas.length; i++) {
-            var etapa = resultado.etapas[i];
-            var etapaDiv = document.createElement("div");
-            etapaDiv.classList.add("etapa");
-
-            var titulo = document.createElement("h2");
-            titulo.textContent = "Etapa " + i ;
-            etapaDiv.appendChild(titulo);
-
-            var tabela = document.createElement("table");
-            tabela.classList.add("matrix-table");
-
-            for (var j = 0; j < etapa.length; j++) {
-                var linha = document.createElement("tr");
-
-                for (var k = 0; k < etapa[j].length; k++) {
-                    var valor = etapa[j][k];
-                    var coluna = document.createElement("td");
-                    coluna.textContent = isNaN(valor) ? '' : valor.toFixed(5);
-                    linha.appendChild(coluna);
-                }
-
-                tabela.appendChild(linha);
-            }
-
-            etapaDiv.appendChild(tabela);
-            etapasDiv.appendChild(etapaDiv);
+      
+        var matrizLDiv = document.getElementById("matriz-l");
+        matrizLDiv.innerHTML = "";
+      
+        var matrizLTitulo = document.createElement("h2");
+        matrizLTitulo.textContent = "Matriz L:";
+        matrizLDiv.appendChild(matrizLTitulo);
+      
+        var matrizLTabela = document.createElement("table");
+        matrizLTabela.classList.add("matrix-table");
+      
+        for (var i = 0; i < resultado.matrizL.length; i++) {
+          var linha = document.createElement("tr");
+      
+          for (var j = 0; j < resultado.matrizL[i].length; j++) {
+            var valor = resultado.matrizL[i][j];
+            var coluna = document.createElement("td");
+            coluna.textContent = isNaN(valor) ? "" : valor.toFixed(5);
+            linha.appendChild(coluna);
+          }
+      
+          matrizLTabela.appendChild(linha);
         }
-
+      
+        matrizLDiv.appendChild(matrizLTabela);
+      
+        var matrizUDiv = document.getElementById("matriz-u");
+        matrizUDiv.innerHTML = "";
+      
+        var matrizUTitulo = document.createElement("h2");
+        matrizUTitulo.textContent = "Matriz U:";
+        matrizUDiv.appendChild(matrizUTitulo);
+      
+        var matrizUTabela = document.createElement("table");
+        matrizUTabela.classList.add("matrix-table");
+      
+        for (var i = 0; i < resultado.matrizU.length; i++) {
+          var linha = document.createElement("tr");
+      
+          for (var j = 0; j < resultado.matrizU[i].length; j++) {
+            var valor = resultado.matrizU[i][j];
+            var coluna = document.createElement("td");
+            coluna.textContent = isNaN(valor) ? "" : valor.toFixed(5);
+            linha.appendChild(coluna);
+          }
+      
+          matrizUTabela.appendChild(linha);
+        }
+      
+        matrizUDiv.appendChild(matrizUTabela);
+        
+        var matrizPDiv = document.getElementById("matriz-p");
+        matrizPDiv.innerHTML = "";
+        
+        var matrizPTitulo = document.createElement("h2");
+        matrizPTitulo.textContent = "Matriz de Permutação:";
+        matrizPDiv.appendChild(matrizPTitulo);
+        
+        var matrizPTabela = document.createElement("table");
+        matrizPTabela.classList.add("matrix-table");
+        
+        for (var i = 0; i < resultado.matrizP.length; i++) {
+          var linha = document.createElement("tr");
+        
+          for (var j = 0; j < resultado.matrizP[i].length; j++) {
+            var valor = resultado.matrizP[i][j];
+            var coluna = document.createElement("td");
+            coluna.textContent = isNaN(valor) ? "" : valor.toFixed(5);
+            linha.appendChild(coluna);
+          }
+        
+          matrizPTabela.appendChild(linha);
+        }
+        
+        matrizPDiv.appendChild(matrizPTabela);
+        
+        for (var i = 0; i < resultado.etapas.length; i++) {
+          var etapa = resultado.etapas[i];
+          var etapaDiv = document.createElement("div");
+          etapaDiv.classList.add("etapa");
+        
+          var titulo = document.createElement("h2");
+          titulo.textContent = "Etapa " + i;
+          etapaDiv.appendChild(titulo);
+        
+          var tabela = document.createElement("table");
+          tabela.classList.add("matrix-table");
+        
+          for (var j = 0; j < etapa.length; j++) {
+            var linha = document.createElement("tr");
+        
+            for (var k = 0; k < etapa[j].length; k++) {
+              var valor = etapa[j][k];
+              var coluna = document.createElement("td");
+              coluna.textContent = isNaN(valor) ? "" : valor.toFixed(5);
+              linha.appendChild(coluna);
+            }
+        
+            tabela.appendChild(linha);
+          }
+        
+          etapaDiv.appendChild(tabela);
+          etapasDiv.appendChild(etapaDiv);
+        }
+        
         var solucoesDiv = document.getElementById("solucoes");
         solucoesDiv.innerHTML = "";
-
+        
         var solucoesTexto = document.createElement("p");
         var solucoesArray = resultado.solucoes.map(function (solucao, index) {
-            return "x" + (index + 1) + " = " + solucao.toFixed(5);
+          return "x" + (index + 1) + " = " + solucao.toFixed(5);
         });
         solucoesTexto.textContent = "Soluções: " + solucoesArray.join(", ");
-        solucoesDiv.appendChild(solucoesTexto);
+        solucoesDiv.appendChild(solucoesTexto);        
+
     } else if (opcao === "2") {
         var chuteInicial = [];
 
@@ -186,53 +262,82 @@ function calcularSistemaLinear() {
 function eliminacaoDeGauss(coeficientes) {
     var etapas = [];
     var n = coeficientes.length;
-
-    etapas.push(JSON.parse(JSON.stringify(coeficientes))); 
-
+  
+    etapas.push(JSON.parse(JSON.stringify(coeficientes)));
+  
+    // Inicializar matrizes L, U e P
+    var matrizL = [];
+    var matrizU = [];
+    var matrizP = [];
+    var colunas = []; // Nomes das colunas
+  
+    for (var i = 0; i < n; i++) {
+      var linhaL = [];
+      var linhaU = [];
+      var linhaP = [];
+      for (var j = 0; j < n; j++) {
+        linhaU.push(coeficientes[i][j]); // Cópia direta da matriz de coeficientes para U
+  
+        if (i === j) {
+          linhaL.push(1);
+          linhaP.push(1);
+        } else {
+          linhaL.push(0);
+          linhaP.push(0);
+        }
+      }
+      linhaU.push(coeficientes[i][n]); // Última coluna 'b'
+      colunas.push('x' + (i + 1)); // Nome da coluna 'xi'
+      colunas.push('b');
+  
+      matrizL.push(linhaL);
+      matrizU.push(linhaU);
+      matrizP.push(linhaP);
+    }
+  
     for (var k = 0; k < n - 1; k++) {
-        for (var i = k + 1; i < n; i++) {
-            var multiplicador = coeficientes[i][k] / coeficientes[k][k];
-
-            for (var j = k; j < n + 1; j++) {
-                coeficientes[i][j] -= multiplicador * coeficientes[k][j];
-            }
+      for (var i = k + 1; i < n; i++) {
+        var multiplicador = matrizU[i][k] / matrizU[k][k];
+        matrizL[i][k] = multiplicador;
+        matrizU[i][k] = 0;
+  
+        for (var j = k + 1; j < n; j++) {
+          matrizU[i][j] -= multiplicador * matrizU[k][j];
         }
-
-        var etapa = JSON.parse(JSON.stringify(coeficientes));
-        etapas.push(etapa);
-
-        if (etapa[k + 1][k] !== 0) {
-            var colunaZero = [];
-
-            for (var i = 0; i < n + 1; i++) {
-                colunaZero.push(0);
-            }
-
-            colunaZero[k] = 1;
-            etapa.splice(k + 1, 0, colunaZero);
-            etapas.push(etapa); 
-        }
+      }
+  
+      var etapa = JSON.parse(JSON.stringify(matrizU));
+      etapas.push(etapa);
     }
-
+  
     var solucoes = [];
-    solucoes[n - 1] = coeficientes[n - 1][n] / coeficientes[n - 1][n - 1];
-
+    solucoes[n - 1] = matrizU[n - 1][n] / matrizU[n - 1][n - 1];
+  
     for (var i = n - 2; i >= 0; i--) {
-        var soma = 0;
-
-        for (var j = i + 1; j < n; j++) {
-            soma += coeficientes[i][j] * solucoes[j];
-        }
-
-        solucoes[i] = (coeficientes[i][n] - soma) / coeficientes[i][i];
+      var soma = 0;
+  
+      for (var j = i + 1; j < n; j++) {
+        soma += matrizU[i][j] * solucoes[j];
+      }
+  
+      solucoes[i] = (matrizU[i][n] - soma) / matrizU[i][i];
     }
-
+  
+    matrizU.forEach(function (linhaU) {
+      linhaU.pop(); // Remove a última coluna da matriz U
+    });
+  
+    colunas.pop(); // Remove o nome da última coluna 'b'
+  
     return {
-        etapas: etapas,
-        solucoes: solucoes
+      etapas: etapas,
+      solucoes: solucoes,
+      matrizL: matrizL,
+      matrizU: matrizU,
+      matrizP: matrizP,
+      colunas: colunas
     };
-}
-
+  }
 
 
 
